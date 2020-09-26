a052435d51d511c88e420e754462a150ff42ebe4
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2020-09-26 16:12:07 +0200

    Update ExampleUnitTest.java

--- app/src/test/java/education/mahmoud/quranyapp/ExampleUnitTest.java
+++ app/src/test/java/education/mahmoud/quranyapp/ExampleUnitTest.java
@@ -12,6 +12,6 @@ import static org.junit.Assert.assertEquals;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        assertEquals(5, 2 + 3);
     }
 }
